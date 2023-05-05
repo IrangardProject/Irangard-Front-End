0bc97549eb0004fe9763355f6d79220483db5042
mahdi <realmahdighazavi@gmail.com>
2023-05-05 22:02:51 +0430

    [feat]: moving the tourCategories to the file constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,18 @@
 export const baseUrl = 'https://api.quilco.ir';
 // export const baseUrl = 'https://api.quilco.ir';
 // export const baseUrl="https://irangard-backend.herokuapp.com"
+
+export const tourCategories = [
+  { label: 'فرهنگی', value: 'cultural' },
+  { label: 'ماجراجویی', value: 'adventural' },
+  { label: 'تفریحی', value: 'entertainment' },
+  { label: 'حیات وحش', value: 'wildlife' },
+  { label: 'آشپزی', value: 'culinary' },
+  { label: 'معنوی', value: 'spiritual' },
+  { label: 'عکاسی', value: 'photography' },
+  { label: 'تاریخی', value: 'historical' },
+  { label: 'طبیعت گردی', value: 'nature' },
+  { label: 'کویری', value: 'desert' },
+  { label: 'قطاری', value: 'train' },
+  { label: 'سایر', value: 'other' },
+];
