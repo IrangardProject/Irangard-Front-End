a693eebaee980a36a5ca1b15135361acb86db022
mahdi <realmahdighazavi@gmail.com>
2023-05-06 13:27:52 +0430

    [feat]: adding the TourCategories react file

--- src/components/TourCategories/TourCategoryCard/index.jsx
+++ src/components/TourCategories/TourCategoryCard/index.jsx
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom';
 
 const TourCategoryCard = ({ tourName, tourImg, categoryId }) => {
   return (
-    <Link to={`tours/?tour_type=${categoryId}`} className="tour-category-card">
+    <Link
+      to={`tours/?tour_type=${categoryId}`}
+      // onClick={() => onTourCategoryClick(categoryId)}
+      className="tour-category-card"
+    >
       <img src={tourImg} alt={tourName} className="tour-category-card__img" />
       <h3 className="tour-category-card__title">{tourName.length > 20 ? tourName.slice(0, 20) + '...' : tourName}</h3>
     </Link>
