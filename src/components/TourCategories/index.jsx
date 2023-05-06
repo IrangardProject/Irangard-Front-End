232dba060492e35c2e39e17172fb84fe69d3de43
mahdi <realmahdighazavi@gmail.com>
2023-05-06 13:28:57 +0430

    [feat]: adding the TourCategoryCard react file

--- src/components/TourCategories/index.jsx
+++ src/components/TourCategories/index.jsx
@@ -15,6 +15,7 @@ import spiritualTours from 'src/assets/images/spiritualTours.jpg';
 import wildlifeTours from 'src/assets/images/wildlifeTours.jpg';
 import trainTours from 'src/assets/images/trainTours.jpg';
 import desertTours from 'src/assets/images/desertTours.jpg';
+import onedayTours from 'src/assets/images/onedayTours.jpg';
 import { Link } from 'react-router-dom';
 
 const tourCardData = [
@@ -26,6 +27,7 @@ const tourCardData = [
   { tourName: tourCategories[5].label, tourImg: spiritualTours },
   { tourName: tourCategories[6].label, tourImg: photographyTours },
   { tourName: tourCategories[7].label, tourImg: historicalTours },
+  { tourName: tourCategories[11].label, tourImg: onedayTours },
   { tourName: tourCategories[8].label, tourImg: natureTours },
   { tourName: tourCategories[9].label, tourImg: desertTours },
   { tourName: tourCategories[10].label, tourImg: trainTours },
@@ -40,37 +42,21 @@ const TourCategories = () => {
     slides: { perView: isMobile ? 1.5 : isTablet ? 2.5 : 4.4, spacing: isMobile ? 4 : 8 },
     rtl: true,
   });
-  return (
-    <div className="tour-categories">
-      <div className="tour-categories__title">
-        <h2>دنبال چه توری می‌گردی؟</h2>
-      </div>
 
-      <div className="tour-categories__cards">
-        {/* <TourCategoryCard tourName={tourCategories[0].label} tourImg={adventuralTours} />
-        <TourCategoryCard tourName={tourCategories[1].label} tourImg={culinaryTours} />
-        <TourCategoryCard tourName={tourCategories[2].label} tourImg={culturalTours} />
-        <TourCategoryCard tourName={tourCategories[3].label} tourImg={entertainmentTours} />
-        <TourCategoryCard tourName={tourCategories[4].label} tourImg={historicalTours} />
-        <TourCategoryCard tourName={tourCategories[5].label} tourImg={natureTours} />
-        <TourCategoryCard tourName={tourCategories[6].label} tourImg={photographyTours} />
-        <TourCategoryCard tourName={tourCategories[7].label} tourImg={spiritualTours} />
-        <TourCategoryCard tourName={tourCategories[8].label} tourImg={wildlifeTours} />
-        <TourCategoryCard tourName={tourCategories[9].label} tourImg={trainTours} />
-        <TourCategoryCard tourName={tourCategories[10].label} tourImg={desertTours} /> */}
-        {/* {tourCardData.map((tourCard, index) => (
-          <TourCategoryCard key={index} tourName={tourCard.tourName} tourImg={tourCard.tourImg} />
-        ))} */}
-        {/* {tourCardData.map((tourCategory, index) => (
-            <Link to={`/tours/?tour_type=${index}`} key={index}>
-              <TourCategoryCard tourName={tourCategory.tourName} tourImg={tourCategory.tourImg} />
-            </Link>
-          ))} */}
-        {tourCardData.map((tourCategory, index) => (
-          <div key={index} className="tour-categories__cards__slide">
-            <TourCategoryCard tourName={tourCategory.tourName} tourImg={tourCategory.tourImg} categoryId={index} />
-          </div>
-        ))}
+  return (
+    <div className="tour-categories-container">
+      <div className="tour-categories">
+        <div className="tour-categories__title">
+          <h2>دنبال چه توری می‌گردی؟</h2>
+        </div>
+
+        <div className="tour-categories__cards">
+          {tourCardData.map((tourCategory, index) => (
+            <div key={index} className="tour-categories__cards__slide">
+              <TourCategoryCard tourName={tourCategory.tourName} tourImg={tourCategory.tourImg} categoryId={index} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
