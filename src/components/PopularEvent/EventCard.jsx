d39700173b701d4810402ec360f2c5948f8a8a65
Seyed Mahdi Hosseini Zade <mahdihosseinizade2000@gmail.com>
2023-05-16 02:39:51 +0330

    [feat]: add EventCard

--- src/components/PopularEvent/EventCard.jsx
+++ src/components/PopularEvent/EventCard.jsx
@@ -3,20 +3,38 @@ import { Link } from 'react-router-dom';
 import defaultXpImg from '../../assets/images/defaultXpImg.png';
 import avatar from '../../assets/images/avatar.png';
 import './styles.scss';
-
-const ExperienceCard = ({ id, imgSrc, title, description, userImgSrc, userName }) => {
+import { Typography } from '@mui/material';
+import PlaceOutlinedIcon from '@mui/icons-material/PlaceOutlined';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import AirportShuttleOutlinedIcon from '@mui/icons-material/AirportShuttleOutlined';
+import PeopleIcon from '@mui/icons-material/People';
+import DateRangeIcon from '@mui/icons-material/DateRange';
+import {convertTimeToPersian} from '../../utils/formatters'
+const ExperienceCard = ({start_time,organizer, city, id, imgSrc, title, description, userImgSrc, userName }) => {
+  console.log('imgSrc: ', imgSrc);
   return (
-    <Link to={`/experiences/${id}`} className="experience-card">
-      <img src={imgSrc || defaultXpImg} alt={title} className="experience-card__img" />
+    <Link to={`/events/${id}`} className="experience-card">
+      <img src={imgSrc.image || defaultXpImg} alt={title} className="experience-card__img" />
       <div className="experience-card__rating-wrapper"></div>
-      <h3 className="experience-card__title">{title?.length > 20 ? title.slice(0, 20) + '...' : title}</h3>
+      <h3 className="experience-card__title">{ title}</h3>
       <p className="experience-card__description">
-      {description?.length > 40 ? description.slice(0, 40) + '...' : description}
+      {/* {description?.length > 40 ? description.slice(0, 60) + '...' : description} */}
       </p>
-      <Link to={`/profile/${userName}`}>
+      <Typography variant="body2" sx={{fontSize:"12px",marginBottom:"10px",display:"flex"}}>
+          <PlaceOutlinedIcon color="primary" sx={{fontSize:"15px"}}/>
+          <span >   محل برگزاری  : &nbsp; </span> {city}
+      </Typography>
+      <Typography variant="body2" sx={{fontSize:"12px",marginBottom:"10px",display:"flex"}}>
+        <DateRangeIcon color="primary" sx={{fontSize:"15px"}}/>
+        <span >  ساعت شروع : &nbsp; </span>{ convertTimeToPersian(start_time)}
+      </Typography>
+      <Link to={`/events/${id}`}>
         <div className="experience-card__footer">
-          <img src={userImgSrc || avatar} alt={userName} className="experience-card__user-img" />
-          <p className="experience-card__user-name">{userName.slice(0, 20)}</p>
+          {/* <img src={userImgSrc || avatar} alt={userName} className="experience-card__user-img" /> */}
+          <Typography variant="body2" sx={{fontSize:"12px",marginBottom:"10px",display:"flex"}}>
+              <PeopleIcon color="primary" sx={{fontSize:"15px"}}/>
+              <span >  برگزارکننده : &nbsp; </span> {organizer.length > 16 ? organizer.slice(0, 16) + '...' : organizer} 
+          </Typography>
         </div>
       </Link>
     </Link>
