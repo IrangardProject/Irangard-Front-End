7d7afdf65284a7f2e2438968828799799c425192
mahdi <realmahdighazavi@gmail.com>
2023-05-23 01:33:18 +0330

    [feat]: adding the ByUser component code and stylings

--- src/components/Tours/TourDetailPage/ByUser/index.jsx
+++ src/components/Tours/TourDetailPage/ByUser/index.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
-import './styles.scss'
+import React, { useState, useEffect } from 'react';
+import './styles.scss';
+import { Link } from 'react-router-dom';
+import useAuth from 'src/context/AuthContext';
+import { baseUrl } from 'src/utils/constants';
 
 
 const ByUser = () => {
-  return <div>ByUser</div>;
+  const auth = useAuth();
+  const user = auth.user;
+  console.log('this is the user: ', auth);
+  if (!user) {
+    return null;
+  }
+
+  return (
+    <div className="by-user">
+    {user && (
+      <Link to={`/profile/${user.username}`}>
+        <div className="by-user-info">
+          <img className="by-user-info__img" src={`${baseUrl}` + user.image} alt={user.username} />
+          <p className="by-user-info__username">ثبت شده توسط {user.username}</p>
+        </div>
+      </Link>
+    )}
+  </div>
+  );
 };
 
 
