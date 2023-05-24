59e919a7b6c90e8bc6e258cf8b2a7d2a4dbd176d
mahdi <realmahdighazavi@gmail.com>
2023-05-25 00:31:23 +0330

    [fix]: fixing the bug with ByUser component in the TourDetailPage

--- src/components/Tours/TourDetailPage/ByUser/index.jsx
+++ src/components/Tours/TourDetailPage/ByUser/index.jsx
@@ -5,6 +5,7 @@ import useAuth from 'src/context/AuthContext';
 import { baseUrl } from 'src/utils/constants';
 
 const ByUser = ({ tourLeader }) => {
+  console.log("the tourLeader's image is: ", tourLeader.image);
   const auth = useAuth();
   const user = auth.user;
   if (!user) {
@@ -16,7 +17,7 @@ const ByUser = ({ tourLeader }) => {
       {user && (
         <Link to={`/profile/${tourLeader.username}`}>
           <div className="by-user-info">
-            {/* <img className="by-user-info__img" src={`${baseUrl}` + user.image} alt={tourLeader.username} /> */}
+            <img className="by-user-info__img" src={tourLeader.image} alt={tourLeader.username} />
             <p className="by-user-info__username">ثبت شده توسط {tourLeader.username}</p>
           </div>
         </Link>
