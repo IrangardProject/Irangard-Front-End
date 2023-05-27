48bb832130a6ab1c6c4f01ea6dc62caf58ab2970
mahdi <realmahdighazavi@gmail.com>
2023-05-27 14:27:25 +0330

    [feat]: coding up and styling the things to do feat

--- src/components/PlaceThingsToDoSearch/index.jsx
+++ src/components/PlaceThingsToDoSearch/index.jsx
@@ -3,40 +3,169 @@ import toast, { Toaster } from 'react-hot-toast';
 import { Modal } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import './styles.scss';
+import { IoLocationOutline } from 'react-icons/io5';
+import { useEffect } from 'react';
+import IranStates from 'src/assets/data/IranStates.json';
+import { TbZoomCancel } from 'react-icons/tb';
+import IranStateKeys from 'src/assets/data/IranStateKeys.json';
 
 const PlaceThingsToDoSearch = ({ open, setOpen }) => {
-  const [query, setQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [queryResults, setQueryResults] = useState([]);
+
   const navigate = useNavigate();
+  const searchInputRef = useRef(null);
+
+  useEffect(() => {
+    if (searchQuery.length >= 1) {
+      const results = IranStates.filter(state => {
+        return state.label.includes(searchQuery);
+      });
+      console.log('results:', results);
+      setQueryResults(results);
+    }
+  }, [searchQuery]);
+
+  const searchQueryHandler = e => {
+    setSearchQuery(e.target.value);
+  };
 
   const searchHandler = e => {
     e.preventDefault();
-    console.log("submiting result of where to go, naigating to: ", `/things-to-do&where=${query}`)
-    if (query.length < 3) {
+    console.log('submitting result of where to go, navigating to:', `/things-to-do&where=${searchQuery}`);
+    if (searchQuery.length < 3) {
       toast.error('حداقل سه حرف وارد کنید');
       return;
     }
-    navigate(`/things-to-do?where=${query}`);
+    navigate(`/things-to-do?where-to=${searchQuery}`);
     setOpen(false);
+  };
+
+  const suggestionsSearchHandler = search => {
+    if (search.length < 3) {
+      toast.error('حداقل سه حرف وارد کنید');
+      return;
+    }
+    navigate(`/things-to-do?where-to=${search}`);
   };
 
   const modalCloseHandler = () => {
     setOpen(false);
   };
+
+  const handleKeyPress = e => {
+    if (e.key === 'Enter') {
+      searchHandler(e);
+    }
+  };
+
   return (
-    <Modal
-      className="wrapper"
-      open={open}
-      onClose={modalCloseHandler}
-    >
-      <div className="wrapper__container">
-        <form onSubmit={searchHandler} className="wrapper__container__search-bar">
+    <Modal className="things-to-do-wrapper" open={open} onClose={modalCloseHandler}>
+      <div className="things-to-do-wrapper__container">
+        <div className="things-to-do-wrapper__container__search-form">
           <input
-            value={query}
-            onChange={e => setQuery(e.target.value)}
-            className="wrapper__container__search-bar__search-box"
-            placeholder="جست‌وجو برای مقصد..."
+            ref={searchInputRef}
+            value={searchQuery}
+            onChange={searchQueryHandler}
+            onKeyPress={handleKeyPress}
+            className="things-to-do-wrapper__container__search-form__search-box"
+            placeholder="به کجا؟..."
           />
-        </form>
+        </div>
+        <hr className="things-to-do-wrapper__container__hr" />
+        {(searchQuery.length < 1 || queryResults.length > 0) && (
+          <div className="things-to-do-wrapper__container__suggestions-title">
+            <span className="things-to-do-wrapper__container__suggestions-title__title">مقاصد پیشنهادی:</span>
+          </div>
+        )}
+        {/* <div className="things-to-do-wrapper__container__suggestions-title">
+            <span className="things-to-do-wrapper__container__suggestions-title__title">مقصدی یافت نشد!</span>
+          </div> */}
+        {searchQuery.length >= 1 && queryResults.length === 0 && (
+          <div className="no-things-to-do-wrapper">
+            <div className="no-things-to-do">
+              <TbZoomCancel style={{ fontSize: '48px', color: '#04aa6c' }} />
+              <h5 className="no-things-to-do__title">مقصدی یافت نشد!</h5>
+            </div>
+          </div>
+        )}
+        {searchQuery.length < 1 && (
+          <div className="things-to-do-wrapper__container__search-suggestions">
+            <div
+              className="things-to-do-wrapper__container__search-suggestions__suggestion"
+              onClick={() => {
+                suggestionsSearchHandler('تهران');
+              }}
+            >
+              <IoLocationOutline className="things-to-do-wrapper__container__search-suggestions__suggestion__icon" />
+              <span className="things-to-do-wrapper__container__search-suggestions__suggestion__title">تهران</span>
+            </div>
+
+            <hr className="things-to-do-wrapper__container__search-suggestions__hr" />
+
+            <div
+              className="things-to-do-wrapper__container__search-suggestions__suggestion"
+              onClick={() => {
+                suggestionsSearchHandler('شیراز');
+              }}
+            >
+              <IoLocationOutline className="things-to-do-wrapper__container__search-suggestions__suggestion__icon" />
+
+              <span className="things-to-do-wrapper__container__search-suggestions__suggestion__title">شیراز</span>
+            </div>
+
+            <hr className="things-to-do-wrapper__container__search-suggestions__hr" />
+
+            <div
+              className="things-to-do-wrapper__container__search-suggestions__suggestion"
+              onClick={() => {
+                suggestionsSearchHandler('اصفهان');
+              }}
+            >
+              <IoLocationOutline className="things-to-do-wrapper__container__search-suggestions__suggestion__icon" />
+
+              <span className="things-to-do-wrapper__container__search-suggestions__suggestion__title">اصفهان</span>
+            </div>
+
+            <hr className="things-to-do-wrapper__container__search-suggestions__hr" />
+
+            <div
+              className="things-to-do-wrapper__container__search-suggestions__suggestion"
+              onClick={() => {
+                suggestionsSearchHandler('شیراز');
+              }}
+            >
+              <IoLocationOutline className="things-to-do-wrapper__container__search-suggestions__suggestion__icon" />
+
+              <span className="things-to-do-wrapper__container__search-suggestions__suggestion__title">یزد</span>
+            </div>
+          </div>
+        )}
+        {searchQuery.length >= 1 && queryResults.length > 0 && (
+          <div className="things-to-do-wrapper__container__search-suggestions">
+            {queryResults.map((result, index) => {
+              return (
+                <>
+                  <div
+                    className="things-to-do-wrapper__container__search-suggestions__suggestion"
+                    key={index}
+                    onClick={() => {
+                      suggestionsSearchHandler(result.label);
+                    }}
+                  >
+                    <IoLocationOutline className="things-to-do-wrapper__container__search-suggestions__suggestion__icon" />
+                    <span className="things-to-do-wrapper__container__search-suggestions__suggestion__title">
+                      {result.label}
+                    </span>
+                  </div>
+                  {index !== queryResults.length - 1 && (
+                    <hr className="things-to-do-wrapper__container__search-suggestions__hr" />
+                  )}
+                </>
+              );
+            })}
+          </div>
+        )}
       </div>
     </Modal>
   );
