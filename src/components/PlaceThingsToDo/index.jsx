ec534c7a69cb70933aa7206dbd10a8936db6407a
mahdi <realmahdighazavi@gmail.com>
2023-05-29 22:37:29 +0330

    [refactor]: refactoring the page with Tours and Events cards

--- src/components/PlaceThingsToDo/index.jsx
+++ src/components/PlaceThingsToDo/index.jsx
@@ -12,12 +12,13 @@ import useAuth from 'src/context/AuthContext';
 import './styles.scss';
 import toast, { Toaster } from 'react-hot-toast';
 import { useSearchParams, useLocation } from 'react-router-dom';
-import TourSlider from './TourSlider';
-import EventSlider from './EventSlider';
+import TourCard from 'src/components/Tours/TourCard';
+import EventCard from 'src/components/Events/EventCard';
 import PlaceCard from './PlaceCard';
 import { TbZoomCancel } from 'react-icons/tb';
 
 const PlaceThingsToDo = () => {
+  const auth = useAuth();
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
   const location = useLocation();
@@ -57,80 +58,95 @@ const PlaceThingsToDo = () => {
   };
 
   return (
-    <Layout title="ایران‌گردی">
-      {loading && <Loader />}
-      {!loading && availableEvents.length === 0 && availablePlaces.length === 0 && availableTours.length === 0 && (
-        <>
-          <div className="things-to-do__header">
-            <div className="things-to-do__header__title">
-              <h2>ایران‌گردی در {whereToParam}</h2>
-            </div>
+    <>
+      {!auth.isLoggedIn && (
+        <Layout title="ایران‌گردی">
+          <div className="things-to-do__not-logged-in">
+            <p>برای مشاهده این بخش باید وارد حساب کاربری خود شوید.</p>
           </div>
-          <hr className="things-to-do__hr" />
-          <div className="no-available-things-to-do-wrapper">
-            <div className="no-available-things-to-do">
-              <TbZoomCancel style={{ fontSize: '88px', color: '#04aa6c' }} />
-              <h3 className="no-available-things-to-do__title">موردی یافت نشد!</h3>
-            </div>
-          </div>
-        </>
+        </Layout>
       )}
-      {/* availableEvents.length !== 0 && 
+      {auth.isLoggedIn && (
+        <Layout title="ایران‌گردی">
+          {loading && <Loader />}
+          {!loading && availableEvents.length === 0 && availablePlaces.length === 0 && availableTours.length === 0 && (
+            <>
+              <div className="things-to-do__header">
+                <div className="things-to-do__header__title">
+                  <h2>ایران‌گردی در {whereToParam}</h2>
+                </div>
+              </div>
+              <hr className="things-to-do__hr" />
+              <div className="no-available-things-to-do-wrapper">
+                <div className="no-available-things-to-do">
+                  <TbZoomCancel style={{ fontSize: '88px', color: '#04aa6c' }} />
+                  <h3 className="no-available-things-to-do__title">موردی یافت نشد!</h3>
+                </div>
+              </div>
+            </>
+          )}
+          {/* availableEvents.length !== 0 && 
       availablePlaces.length !== 0 && 
       availableTours.length !== 0 &&  */}
-      {!loading && (
-        <div className="things-to-do">
-          <div className="things-to-do__header">
-            <div className="things-to-do__header__title">
-              <h2>ایران‌گردی در {whereToParam}</h2>
+          {!loading && (
+            <div className="things-to-do">
+              <div className="things-to-do__header">
+                <div className="things-to-do__header__title">
+                  <h2>ایران‌گردی در {whereToParam}</h2>
+                </div>
+              </div>
+              <hr className="things-to-do__hr" />
+              <div className="things-to-do__content">
+                {availableTours.length > 0 && (
+                  <>
+                    <div className="things-to-do__content__tours">
+                      <h5 onClick={seeAllToursHandler}>مشاهده همه</h5>
+
+                      <h3>راه‌های سفر به {whereToParam}</h3>
+                      <div className="things-to-do__content__tours__cards">
+                        {availableTours.length > 3
+                          ? availableTours.slice(0, 3).map(tour => <TourCard tour={tour} />)
+                          : availableTours.map(tour => <TourCard tour={tour} />)}
+                      </div>
+                    </div>
+                    <hr className="things-to-do__hr" />
+                  </>
+                )}
+                {availablePlaces.length > 0 && (
+                  <>
+                    <div className="things-to-do__content__places">
+                      <h5 onClick={seeAllPlacesHandler}>مشاهده همه</h5>
+                      <h3>جاذبه‌های دیدنی {whereToParam}</h3>
+                      <div className="things-to-do__content__places__cards">
+                        {availablePlaces.length > 3
+                          ? availablePlaces.slice(0, 3).map(place => <PlaceCard place={place} />)
+                          : availablePlaces.map(place => <PlaceCard place={place} />)}
+                      </div>
+                    </div>
+                    <hr className="things-to-do__hr" />
+                  </>
+                )}
+
+                {availableEvents.length > 0 && (
+                  <>
+                    <div className="things-to-do__content__events">
+                      <h5 onClick={seeAllEventsHandler}>مشاهده همه</h5>
+
+                      <h3>رویداد‌های مهم در {whereToParam}</h3>
+                      <div className="things-to-do__content__events__cards">
+                        {availableEvents.length > 3
+                          ? availableEvents.slice(0, 3).map(event => <EventCard event={event} />)
+                          : availableEvents.map(event => <EventCard event={event} />)}
+                      </div>
+                    </div>
+                  </>
+                )}
+              </div>
             </div>
-          </div>
-          <hr className="things-to-do__hr" />
-          <div className="things-to-do__content">
-            {availableTours.length > 0 && (
-              <>
-                <div className="things-to-do__content__tours">
-                  <h5 onClick={seeAllToursHandler}>مشاهده همه</h5>
-
-                  <h3>راه‌های سفر به {whereToParam}</h3>
-                  <div className="things-to-do__content__tours__slider">
-                    <TourSlider tours={availableTours} />
-                  </div>
-                </div>
-                <hr className="things-to-do__hr" />
-              </>
-            )}
-            {availablePlaces.length > 0 && (
-              <>
-                <div className="things-to-do__content__places">
-                  <h5 onClick={seeAllPlacesHandler}>مشاهده همه</h5>
-                  <h3>جاذبه‌های دیدنی {whereToParam}</h3>
-                  <div className="things-to-do__content__places__cards">
-                    {availablePlaces.length > 3
-                      ? availablePlaces.slice(0, 3).map(place => <PlaceCard place={place} />)
-                      : availablePlaces.map(place => <PlaceCard place={place} />)}
-                  </div>
-                </div>
-                <hr className="things-to-do__hr" />
-              </>
-            )}
-
-            {availableEvents.length > 0 && (
-              <>
-                <div className="things-to-do__content__events">
-                  <h5 onClick={seeAllEventsHandler}>مشاهده همه</h5>
-
-                  <h3>رویداد‌های مهم در {whereToParam}</h3>
-                  <div className="things-to-do__content__events__slider">
-                    <EventSlider events={availableEvents} />
-                  </div>
-                </div>
-              </>
-            )}
-          </div>
-        </div>
+          )}
+        </Layout>
       )}
-    </Layout>
+    </>
   );
 };
 
